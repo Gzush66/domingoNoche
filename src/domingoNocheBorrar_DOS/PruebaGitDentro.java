d0d43b2c38e99f4fc32dbec4814b01d48b7e7ad9
Juan Jesus Lozano <garbagetin1@gmail.com>
2022-05-16 01:21:51 +0200

    se añade solo el archivo java y el gitignore

---
+++ src/domingoNocheBorrar_DOS/PruebaGitDentro.java
@@ -0,0 +1,11 @@
+package domingoNocheBorrar_DOS;
+
+public class PruebaGitDentro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("creando un git dentro del projecto");
+
+	}
+
+}
